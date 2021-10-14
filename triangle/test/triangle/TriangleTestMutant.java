3e2e5fd7f10281ed3f68ca616f98ef7153375895
Philip Hopen <phopen@umass.edu>
2021-10-14 17:23:21 -0400

    Completed Mutant Coverage Testing

--- triangle/test/triangle/TriangleTestMutant.java
+++ triangle/test/triangle/TriangleTestMutant.java
@@ -32,11 +32,32 @@ public class TriangleTestMutant extends TriangleTestBranch {
 		super.testScalene();
         Type actual5 = Triangle.classify(2, 3, 5);
         Type actual6 = Triangle.classify(2, 5, 3);
+		Type actual7 = Triangle.classify(7, 2, 4);
+		Type actual8 = Triangle.classify(5, 2, 3);
+
         Type expected = INVALID;
 
         assertEquals(actual5, expected);
         assertEquals(actual6, expected);
+		assertEquals(actual7, expected);
+		assertEquals(actual8, expected);
 
+		Type actual9 = Triangle.classify(6, 4, 3);
+		expected = SCALENE;
+
+		assertEquals(actual9, expected);
     }
+
+	@Test
+	public void testIsosceles() {
+		super.testIsosceles();
+		Type actual7 = Triangle.classify(3, 3, 6);
+		Type actual8 = Triangle.classify(6, 3, 3);
+		Type actual9 = Triangle.classify(3, 6, 3);
+		Type expected = INVALID;
+		assertEquals(actual7, expected);
+		assertEquals(actual8, expected);
+		assertEquals(actual9, expected);
+	}
 
 }
