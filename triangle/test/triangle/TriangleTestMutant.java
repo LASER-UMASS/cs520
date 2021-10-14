ca3e7d223cc96486c7c57dbb77a2af79059b5764
superben486 <ben@thesupers.org>
2021-10-14 11:54:30 -0400

    Added mutant test suite file

---
+++ triangle/test/triangle/TriangleTestMutant.java
@@ -0,0 +1,11 @@
+package triangle;
+
+import static org.junit.Assert.*;
+import static triangle.Triangle.Type;
+import static triangle.Triangle.Type.*;
+
+import org.junit.Test;
+
+public class TriangleTestMutant extends TriangleTestBranch {
+	
+}
